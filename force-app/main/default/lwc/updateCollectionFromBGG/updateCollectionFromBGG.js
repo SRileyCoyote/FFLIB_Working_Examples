ed50e4d739ab2f4f6b2290e5c63410097edf8bf3
Sean Riley <sriley@cloudqnect.com>
2025-01-03 11:34:29 -0500

    Add Functionality to Update Library Entry Information based on given Source

--- force-app/main/default/lwc/updateCollectionFromBGG/updateCollectionFromBGG.js
+++ force-app/main/default/lwc/updateCollectionFromBGG/updateCollectionFromBGG.js
@@ -1,12 +1,17 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import UpdateGameRecordFromBGG from '@salesforce/apex/UpdateFromBGGController.updateGameRecordFromBGG';
+import UpdateBGLERecordFromBGG from '@salesforce/apex/UpdateFromBGGController.updateLibraryEntryRecordFromBGG';
 
 // Fields to query from the record
-const FIELDS = ['Board_Games__c.BGG_ID__c'];
+const FIELDS = [
+                'BG_Library_Entry__c.Source__c', 
+                'BG_Library_Entry__c.Source_Type__c', 
+                'BG_Library_Entry__c.BGG_Id__c',
+                'BG_Library_Entry__c.BoardGameName__c'
+            ];
 
-export default class updateGameFromBGG extends LightningElement {
+export default class UpdateCollectionFromBGG extends LightningElement {
     @api recordId; // Automatically provided
     recordData;
 
@@ -14,7 +19,15 @@ export default class updateGameFromBGG extends LightningElement {
     @wire(getRecord, {recordId: '$recordId', fields: FIELDS})
     wiredRecord({error, data}){
         if (data) {
-            this.recordData = data;
+            // Each Field Value from Data must be mapped into the recordData variable
+            this.recordData = {
+                Id: this.recordId,
+                BoardGameName__c: data.fields.BoardGameName__c.value,
+                Source__c: data.fields.Source__c.value,
+                Source_Type__c: data.fields.Source_Type__c.value,
+                BGG_Id__c: data.fields.BGG_Id__c.value
+            };
+;
         } else if (error) {
             this.showToast(
                 'Error Fetching Data',
@@ -26,8 +39,8 @@ export default class updateGameFromBGG extends LightningElement {
 
     //Invoke method required for Headless Actions
     @api invoke() {
-        //Call Apex Controller passing along record ID and the BGG_ID__c values
-        UpdateGameRecordFromBGG({ recordId: this.recordId, bggId: this.recordData.fields.BGG_ID__c.value})
+        //Call Apex Controller passing along record ID and the Record Data value
+        UpdateBGLERecordFromBGG({ recordId: this.recordId, bgle: this.recordData})
             .then(result => {
                 // Handle successful update
                 this.showToast('Import Successful', result, 'success');
@@ -47,5 +60,4 @@ export default class updateGameFromBGG extends LightningElement {
         });
         this.dispatchEvent(toastEvent);
     }
-
 }
